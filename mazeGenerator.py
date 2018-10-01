ef493627aee0e91e80ba5bd2e6831a78b17b16e6
Daniel Zhu <dzdude77@gmail.com>
2018-10-01 10:10:52 -0400

    soft gradient/better color changing algo
    added option to reset color when visiting new pixel
    changed the way change_colors works to not get stuck in black

--- mazeGenerator.py
+++ mazeGenerator.py
@@ -5,11 +5,11 @@ import sys
 sys.setrecursionlimit(3000)
 
 class Maze:
-    def __init__(self, w, h, c, f, save_loc):
+    def __init__(self, w, h, color, order, factor, save_loc):
         self.wcells, self.hcells = w, h
         self.w, self.h = w * 2 + 1, h * 2 + 1
-        self.color, self.factor, self.save_loc = c, f, save_loc
-        self.colors, self.change = [255, 255, 255], [f] * 3
+        self.color, self.factor, self.order, self.save_loc = color, factor, order, save_loc
+        self.colors, self.change = [255, 255, 255], [factor] * 3
 
         self.im = Image.new('RGBA', (self.w, self.h), color = 'black')
         self.pix = self.im.load()
@@ -27,8 +27,12 @@ class Maze:
         first = random.choice(unvisited)
         self.visit(first[0], first[1], unvisited)
 
-        self.color_pix(random.randint(0, self.wcells - 1) * 2 + 1, 0)
-        self.color_pix(random.randint(0, self.wcells - 1) * 2 + 1, self.h - 1)
+        start_x = random.randint(0, self.wcells - 1) * 2 + 1
+        start_y = random.randint(0, self.wcells - 1) * 2 + 1
+        self.colors[0], self.colors[1], self.colors[2], none = self.pix[start_x, 1]
+        self.color_pix(start_x, 0)
+        self.colors[0], self.colors[1], self.colors[2], none = self.pix[start_y, self.h-2]
+        self.color_pix(start_y, self.h - 1)
 
         self.im.save(self.save_loc)
         # if input("enlarged version (y/n): ") == 'y':
@@ -39,6 +43,7 @@ class Maze:
         if self.color:
             self.change_colors()
         unvisited.remove([x, y])
+        this_colors = [self.colors[0], self.colors[1], self.colors[2]]
 
         directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
         random.shuffle(directions)
@@ -47,6 +52,8 @@ class Maze:
             xcurr = x + xdir
             ycurr = y + ydir
             if [xcurr, ycurr] in unvisited:
+                if self.order:
+                    self.colors = [this_colors[0], this_colors[1], this_colors[2]]
                 self.color_pix(x * 2 + 1 + xdir, y * 2 + 1 + ydir)
                 if self.color:
                     self.change_colors()
@@ -58,9 +65,9 @@ class Maze:
     def change_colors(self):
         for i in range(3):
             if self.colors[i] >= 255:
-                self.change[i] -= self.factor * 2 * (i + 1)
-            elif self.colors[i] <= 0:
-                self.change[i] += self.factor * 2 * (i + 1)
+                self.change[i] = -(self.factor  + (i + 1))
+            elif self.colors[i] <= 30:
+                self.change[i] = self.factor + (i + 1)
             self.colors[i] += self.change[i]
 
     def resize(self, factor):
@@ -69,17 +76,17 @@ class Maze:
         for x in range(large_im.width):
             for y in range(large_im.height):
                 large_pix[x, y] = self.pix[int(x / factor), int(y / factor)]
-        large_im.save('mazelarge.png')
+        large_im.save('mazes\mazelarge.png')
 
 def prompt():
     w = int(input("width: "))
     h = int(input("height: "))
-    c = input("colored path creation (y/n): ") == 'y'
-    save_loc = "maze.png"
+    c = input("colored path (y/n): ") == 'y'
+    save_loc = "mazes\maze.png"
     if c:
-        return Maze(w, h, c, int(input("color gradient factor: ")), save_loc)
-    return Maze(w, h, c, 0, save_loc)
+        return Maze(w, h, c, input("based on order of creation (y/n): ") != 'y', int(input("color gradient factor: ")), save_loc)
+    return Maze(w, h, c, False, 0, save_loc)
 
-#m = Maze(20, 20, True, 1, save_loc)
-#m = prompt()
-#m.generate()
+# m = Maze(70, 70, True, False, 1, "mazes\maze.png")
+m = prompt()
+m.generate()
